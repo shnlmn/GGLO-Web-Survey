64548d816fedaa0dfa3784f308c98c76292df6c1
shaleaman <magnetscoil@gmail.com>
2021-04-28 18:52:20 -0700

    add ja logo

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <v-container>
-    <v-img src="/img/GGLO_logo_dark.png" max-width="200" max-height="200" class="mx-auto" id="logo"></v-img>
+    <v-img src="/img/JA-Logo-Round-Black-Transparent.png" max-width="200" max-height="200" class="mx-auto" id="logo"></v-img>
     </v-container>
   </div>
 </template>
